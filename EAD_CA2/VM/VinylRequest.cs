2691253533d64d0aabdece382ce807ea54a6ad22
Dylan Nelson <dylad141@gmail.com>
2021-04-06 22:22:51 +0100

    Revert "delete old for new files"
    This reverts commit 088218a8cff1231656112228e1db7a1fb7dfc239.

---
+++ EAD_CA2/VM/VinylRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAD_CA2.VM
+{
+    public class VinylRequest
+    {
+        [Key]
+        public int VinylID { get; set; }
+
+        [Required]
+        public string VinylName { get; set; }
+
+        [Required]
+        public string Artist { get; set; }
+
+        [Required]
+        public string VinylDescription { get; set; }
+
+
+        [Required]
+        public DateTime ReleaseYear { get; set; }
+
+        [Required]
+        public  int GenreID { get; set; }
+    }
+}
